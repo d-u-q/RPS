c3c9c57c48f2e3346ba562e0a384e9777ce69e18
Kian Murdock <kmurdoc4@uncc.edu>
2018-07-10 16:14:33 -0400

    Initial commit

---
+++ src/rps/RPS.java
@@ -0,0 +1,48 @@
+package rps;
+import java.util.Scanner;
+/**
+ *
+ * @author kianm
+ */
+public class RPS {
+
+    public static void main(String[] args) {
+        String cMove = "";
+        String pMove = "";
+        Boolean validMoveMade = false;
+        Scanner kb = new Scanner(System.in);
+        
+        System.out.println("Let's play rock paper scissors!");
+        while(!validMoveMade){
+            System.out.println("Make your move!");
+            pMove = kb.nextLine();
+        
+            //input validation
+            if(pMove.equalsIgnoreCase("rock") || pMove.equalsIgnoreCase("paper") || pMove.equalsIgnoreCase("scissors")){
+                validMoveMade = true;                
+            }
+            else {
+                System.out.println("ERROR: invalid move.");
+            }             
+        }
+        cMove = generateMove();
+            
+        System.out.println(cMove + ", " + pMove);
+    }
+    
+    private static String generateMove(){
+        String move = "you shouldn't see this";
+        
+        //very advanced machine learning algorhythm ft. ai & blockchain technologies
+        double rNum = Math.random();
+        if (rNum < .33){
+            move = "Rock";}
+        else if (rNum < .66){
+            move = "Paper";}
+        else{
+            move = "Scissors";}
+        
+        return move;
+    }   
+    
+}
